88758bf7357a7d96a95934c29d65f93b118f14e7
Zeno Zeng <zenoofzeng@gmail.com>
2018-03-10 20:35:37 +0800

    update parser

--- lib/build.ts
+++ lib/build.ts
@@ -1,14 +1,17 @@
 import * as fs from 'fs'
 import { promisify } from 'util'
+import * as yaml from 'js-yaml'
+import { Parser, Font } from './parser'
 
 function readFile(path: string) {
     return promisify(fs.readFile)(path, {encoding: 'UTF-8'})
 }
 
-async function build() {
+(async function () {
     const headerTemplate = await readFile('template/header.css')
     const fontsYAML = await readFile('src/fonts.yml')
     const enFontsYAML = await readFile('src/fonts.en.yml')
-}
-
-build()
+    const fonts = yaml.load(fontsYAML) as  Font[]
+    const enFonts = yaml.load(enFontsYAML) as Font[]
+    const results = new Parser(fonts, enFonts).parse()
+})()
--- lib/parser.ts
+++ lib/parser.ts
@@ -1,14 +1,20 @@
-interface GenericFamily {
+export interface GenericFamily {
     name: string
     fallbackGenericFamily: string
     className: string
 }
 
-interface Font {
+export interface Font {
     name: string
-    alias: string[]
-    genericFamily: GenericFamily
-    platfrom: string[]
+    alias: string[] | string
+    genericFamilyName: string
+    platfrom: string[] | string
+    note: string[] | string
+}
+
+export interface ParseResult extends GenericFamily {
+    fonts: Font[]
+    cssFontFamilies: string[]
 }
 
 const genericFamilies: GenericFamily[] = [
@@ -34,6 +40,22 @@ const genericFamilies: GenericFamily[] = [
     },
 ]
 
-class Parser {
-    
+export class Parser {
+    constructor(private fonts: Font[], private enFonts: Font[]) {}
+
+    parseGenericFamily(genericFamily: GenericFamily) : ParseResult {
+        let result = {} as ParseResult
+        Object.assign(result, genericFamily)
+        const filter = (font: Font) => font.genericFamilyName == genericFamily.name 
+        result.fonts = this.enFonts.filter(filter)
+        result.fonts = result.fonts.concat(this.fonts.filter(filter))
+        result.cssFontFamilies = ([] as string[]).concat(...result.fonts.map((font) => font.alias)) // type of font.alias is string[] | string, flatten here
+        result.cssFontFamilies.push(genericFamily.fallbackGenericFamily)
+        return result
+    }
+
+    parse() : ParseResult[] {
+        return genericFamilies.map((genericFamily) => this.parseGenericFamily(genericFamily))
+    }
+
 }
